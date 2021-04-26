5b27cf286954add6a0a14cae3360cef997936ef8
X <70487315+XWasHere@users.noreply.github.com>
2021-04-26 15:28:05 -0400

    Update cocktail-shaker.js

--- cocktail-shaker.js
+++ cocktail-shaker.js
@@ -10,14 +10,36 @@ chart.set(D)
 
 Layout.setRoot(new VerticalLayout([chart,tracer]))
 
-function cocktailShakerSort(A) {
+function swap(i1, i2) {
+    let a = D[i1]
+    let b = D[i2]
+    D[i2] = a;
+    D[i1] = b;
+}
+
+function cocktailShakerSort() {
     do {
         swapped = false
         for (i = 0; i<D.length-2; i++) {
+            chart.select(i)
+            chart.select(i+1)
+            tracer.select(i)
+            tracer.select(i+1)
+            
             if (D[i] > D[i + 1]) { // test whether the two elements are in the wrong order
-                swap(A[i], A[i + 1]) // let the two elements change places
+                swap(i,i + 1) // let the two elements change places
                 swapped = true
+                chart.patch(i, D[i])
+                chart.patch(i+1,D[i+1])
             }
+            
+            Tracer.delay()
+            chart.deselect(i)
+            chart.deselect(i+1)
+            tracer.deselect(i)
+            tracer.deselect(i+1)
+            chart.depatch(i)
+            chart.depatch(i+1)
         }
         if (!swapped) {
             // we can exit the outer loop here if no swaps occurred.
@@ -25,10 +47,25 @@ function cocktailShakerSort(A) {
         }
         swapped = false
         for (i = 0; i<D.length-2; i++) {
-            if (A[i] > A[i + 1] ) {
-                swap(A[i], A[i + 1])
+            chart.select(i)
+            chart.select(i+1)
+            tracer.select(i)
+            tracer.select(i+1)
+            
+            if (D[i] > D[i + 1] ) {
+                swap(i, i + 1)
                 swapped = true
+                chart.patch(i, D[i])
+                chart.patch(i+1,D[i+1])
             }
+            
+            Tracer.delay()
+            chart.deselect(i)
+            chart.deselect(i+1)
+            tracer.deselect(i)
+            tracer.deselect(i+1)
+            chart.depatch(i)
+            chart.depatch(i+1)
         }
     } while (swapped) // if no elements have been swapped, then the list is sorted
 }
